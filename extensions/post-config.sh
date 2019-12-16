75b7fac8b78ef903933a2160a1ea0db9dc7f37ac
Patrick Del Bello <45429977+imptrck@users.noreply.github.com>
2019-12-16 17:56:57 -0300

    Update post-config.sh

--- extensions/post-config.sh
+++ extensions/post-config.sh
@@ -1,4 +1,4 @@
-echo "Executing"
+echo "Executing" >> /tmp/test
 JAVA_OPTS="-Djava.security.properties=/opt/eap/extensions/bcfips.security.properties" $JBOSS_HOME/bin/jboss-cli.sh --file=/opt/eap/extensions/enable-bcfips.cli
 
 echo "Finished executing" >> /tmp/test
